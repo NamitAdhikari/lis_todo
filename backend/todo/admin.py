9947d4ba5714b03161273a05b6e9bad4fd609985
Namit Adhikari <official.namit321@gmail.com>
2022-02-01 15:49:51 +0545

    Backend Todo Complete

--- backend/todo/admin.py
+++ backend/todo/admin.py
@@ -1,3 +1,11 @@
+from pyexpat import model
 from django.contrib import admin
 
-# Register your models here.
+from .models import Todo
+
+
+class TodoAdmin(admin.ModelAdmin):
+    model = Todo
+    list_display = ['description', 'target_date', ]
+
+admin.site.register(Todo, TodoAdmin)
--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    description = models.TextField()
+    target_date = models.DateField()
+
+    def __str__(self):
+        return self.description
